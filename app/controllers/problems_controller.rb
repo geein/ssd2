14e44dab76770a05c6bd81e7a7e07a84915d056e
geein <lim_jiin@naver.com>
2016-11-10 16:08:16 +0900

    beforejs

--- app/controllers/problems_controller.rb
+++ app/controllers/problems_controller.rb
@@ -1,9 +1,49 @@
 class ProblemsController < ApplicationController
 	before_action :authenticate_user!
 
+	def index
+		@problems = Problem.all
+	end
+
+	def new	
+	end
+
+	def create
+		@problem = Problem.new(problem_params)
+		@problem.save
+  		redirect_to @problem
+	end
+
+	def show 
+		@problem = Problem.find(params[:id])	
+	end
+
+	def edit
+	end
+
+	def update
+	end
+
+	def today
+	end
+
+	def newstep
+		@problem = Problem.new
+		@problems = Problem.all
+	end
+
+	def aboutus
+	end
+	def settings
+	end
+
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
   end
+
+  def problem_params
+    params.require(:problem).permit(:text)
+  end  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
   root 'welcome#index'
-  devise_for :users, controllers: { sessions: 'users/sessions' }
+  resources :problems
+
+  devise_for :users
+
+  get '/today' => 'problems#today'
+  get '/newstep' =>'problems#newstep'
+  get '/aboutus' =>'problems#aboutus'
+  get '/settings' =>'problems#settings'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
